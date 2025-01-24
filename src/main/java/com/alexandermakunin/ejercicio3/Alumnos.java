39cacdd86645dcd44b6ad13a80bdfa6ab72cb842
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2025-01-24 10:05:59 +0100

    Update Alumnos.java

--- src/main/java/com/alexandermakunin/ejercicio3/Alumnos.java
+++ src/main/java/com/alexandermakunin/ejercicio3/Alumnos.java
@@ -7,4 +7,27 @@ public class Alumnos {
     private String nacimiento;
     private String grupo;
     private int telefono;
+
+    public Asignatura(String nia, String nombre, String apellidos, String nacimiento, String grupo, int telefono) {
+        this.nia = nia;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nacimiento = nacimiento;
+        this.grupo = grupo;
+        this.telefono = telefono;
+    }
+
+    public Asignatura() {
+        this ("A1234567M", "NOMBRE", "APELLIDOS", "XX-XX-XXXX", "GRUPO", "+11234567");
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" +
+                "nia=" + nia + '\'' +
+                ", nombre=" + nombre +
+                ", apellidos=" + apellidos + '\'' +
+           		"fecha nacimiento=" 
+                '}';
+    }
 }
