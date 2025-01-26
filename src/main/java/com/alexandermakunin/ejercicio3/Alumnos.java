6df4c2f73a7b3d134c1c9ee6b76939408343120f
Alexander Makunin <amakunin7@gmail.com>
2025-01-26 04:23:25 +0100

    me da flojera la 3

--- src/main/java/com/alexandermakunin/ejercicio3/Alumnos.java
+++ src/main/java/com/alexandermakunin/ejercicio3/Alumnos.java
@@ -8,7 +8,7 @@ public class Alumnos {
     private String grupo;
     private int telefono;
 
-    public Asignatura(String nia, String nombre, String apellidos, String nacimiento, String grupo, int telefono) {
+    public Alumnos(String nia, String nombre, String apellidos, String nacimiento, String grupo, int telefono) {
         this.nia = nia;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -17,17 +17,67 @@ public class Alumnos {
         this.telefono = telefono;
     }
 
-    public Asignatura() {
-        this ("A1234567M", "NOMBRE", "APELLIDOS", "XX-XX-XXXX", "GRUPO", "+11234567");
+    public Alumnos() {
+        this ("A1234567M", "NOMBRE", "APELLIDOS", "XX-XX-XXXX", "GRUPO", 11234567);
+    }
+
+    public String getNia() {
+        return nia;
+    }
+
+    public void setNia(String nia) {
+        this.nia = nia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
     }
 
     @Override
     public String toString() {
-        return "Asignatura{" +
-                "nia=" + nia + '\'' +
-                ", nombre=" + nombre +
-                ", apellidos=" + apellidos + '\'' +
-           		"fecha nacimiento=" 
+        return "Alumnos{" +
+                "nia='" + nia + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", nacimiento='" + nacimiento + '\'' +
+                ", grupo='" + grupo + '\'' +
+                ", telefono=" + telefono +
                 '}';
     }
 }
--- src/main/java/com/alexandermakunin/ejercicio3/Principal.java
+++ src/main/java/com/alexandermakunin/ejercicio3/Principal.java
@@ -1,12 +1,15 @@
 package com.alexandermakunin.ejercicio3;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
 
 public class Principal {
     public static Scanner leer = new Scanner(System.in);
+    public static List<Alumnos> alumno = new ArrayList<>();
     public static void main(String[] args) {
         int seleccion;
-
         do {
             System.out.println("*********************");
             System.out.println("** GESTIÓN ALUMNOS **");
@@ -14,21 +17,114 @@ public class Principal {
             System.out.println("1. Nuevo alumno …");
             System.out.println("2. Baja de alumno …");
             System.out.println("3. Consultas …");
+            System.out.println("4. Pruebas …");
             System.out.println("------------------------------");
             System.out.println("0. Salir");
             seleccion = Integer.parseInt(leer.nextLine());
             switch (seleccion) {
                 case 1 -> nuevoAlumno();
+                case 2 -> bajaAlumno();
+                case 3 -> consultas();
+                case 4 -> pruebas();
             }
-        } while (seleccion > 3 || seleccion < 0);
+        } while (seleccion != 0);
     }
     public static void nuevoAlumno() {
+        System.out.println("Indique el NIA");
         String NIA = leer.nextLine();
+        if (alumno.getNia().equals(NIA)) {
+            System.out.println("Ese NIA ya esta en uso");
+            return;
+        }
+        System.out.println("Indique el nombre");
         String nombre = leer.nextLine();
+        System.out.println("Indique los apellidos");
         String apellidos = leer.nextLine();
+        System.out.println("Indique la fecha de nacimiento usando este estandar XX-XX-XXXX");
         String nacimiento = leer.nextLine();
+        System.out.println("Indique el grupo de clase");
         String grupo = leer.nextLine();
+        System.out.println("Indique el numero de telefono");
         int telefono = Integer.parseInt(leer.nextLine());
         Alumnos alumno = new Alumnos(NIA,nombre,apellidos,nacimiento,grupo,telefono);
+    }
+    public static void bajaAlumno() {
+        System.out.println("Indique el NIA");
+        String NIA = leer.nextLine();
+        if (alumno.getNia().equals(NIA)) {
+            alumno = null;
+        }
+    }
+    public static void consultas() {
+        int seleccion;
+        do {
+            System.out.println("***************");
+            System.out.println("** CONSULTAS **");
+            System.out.println("***************");
+            System.out.println("1. Por grupo …");
+            System.out.println("2. Por edad ...");
+            System.out.println("3. Por nia ...");
+            System.out.println("4. Por apellidos …");
+            System.out.println("--------------------");
+            System.out.println("0. Volver al menú principal");
+
+            seleccion = Integer.parseInt(leer.nextLine());
+            switch (seleccion) {
+                case 1 -> System.out.println(alumno.getGrupo());
+                case 2 -> System.out.println(alumno.getNacimiento());
+                case 3 -> System.out.println(alumno.getNia());
+                case 4 -> System.out.println(alumno.getApellidos());
+            }
+        } while (seleccion != 0);
+    }
+    public static void pruebas() {
+        System.out.println("Cuantos alumnos?");
+        int cantidad = Integer.parseInt(leer.nextLine());
+        String nia = " ";
+        String nombre = " ";
+        String apellidos = " ";
+        String nacimiento = " ";
+        String grupo = " ";
+        int telefono = 0;
+        for (int i = 0; i < cantidad; i++){
+            Random aleatorio = new Random();
+            int niaAleatorio = aleatorio.nextInt(0,4);
+            switch (niaAleatorio) {
+                case 1 -> nia = "Y123456789M";
+                case 2 -> nia = "Y0987654321F";
+                case 3 -> nia = "Y0000000000A";
+            }
+            int nombreAleatorio = aleatorio.nextInt(0,4);
+            switch (nombreAleatorio) {
+                case 1 -> nombre = "Jose";
+                case 2 -> nombre = "Maria";
+                case 3 -> nombre = "Alien";
+            }
+            int apellidosAleatorio = aleatorio.nextInt(0,4);
+            switch (apellidosAleatorio) {
+                case 1 -> apellidos = "Maria";
+                case 2 -> apellidos = "Jose";
+                case 3 -> apellidos = "Marciano";
+            }
+            int nacimientoAleatorio = aleatorio.nextInt(0,4);
+            switch (nacimientoAleatorio) {
+                case 1 -> nacimiento = "03-12-1985";
+                case 2 -> nacimiento = "06-9-1995";
+                case 3 -> nacimiento = "01-01-0001";
+            }
+            int grupoAleatorio = aleatorio.nextInt(0,4);
+            switch (grupoAleatorio) {
+                case 1 -> grupo = "DAM";
+                case 2 -> grupo = "DAW";
+                case 3 -> grupo = "Vida humana";
+            }
+            int telefonoAleatorio = aleatorio.nextInt(0,4);
+            switch (telefonoAleatorio) {
+                case 1 -> telefono = 834657843;
+                case 2 -> telefono = 326547545;
+                case 3 -> telefono = 999999999;
+            }
+            alumno = new Alumnos(nia,nombre,apellidos,nacimiento,grupo,telefono);
+        }
     }
 }
